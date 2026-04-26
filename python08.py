73aa3e87e7dec37cbdc3a337e055880becaddfaa
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-04-26 23:00:01 +0600

    Add files via upload

---
+++ python08.py
@@ -0,0 +1,10 @@
+num1=90
+num2=80
+num3=70
+
+if num1>num2 and num1>num3:
+    print(f'Number {num1} is greater')
+elif num2>num1 and num2>num3:
+    print(f'Number {num2} is greater')
+else:
+    print(f'Number {num3} is greater')
