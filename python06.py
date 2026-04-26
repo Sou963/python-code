75ce41bbc91afed30b95ea28b241c1403362d91d
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-04-26 22:41:15 +0600

    Add files via upload

---
+++ python06.py
@@ -0,0 +1,13 @@
+#elif
+
+marks=int(input('enter a marks:'))
+if marks>=80:
+    print('A grade')
+
+elif marks>=60:
+    print('B grade ')
+
+elif marks>=40:
+    print('C grade')
+else:
+    print('Fail')
